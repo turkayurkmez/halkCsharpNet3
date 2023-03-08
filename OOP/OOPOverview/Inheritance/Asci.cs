a8d5c0b98a2bf2a7df818c068d3e059415d331a6
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-03-08 10:26:59 +0300

    Inheritance completed

---
+++ OOP/OOPOverview/Inheritance/Asci.cs
@@ -0,0 +1,65 @@
+﻿namespace Inheritance
+{
+    public class Asci
+    {
+        public void Pisir(Yemek yemek)
+        {
+            yemek.Pisir();
+            yemek.SunumYap();
+        }
+    }
+
+    public class Yemek
+    {
+        public int PismeSuresi { get; set; }
+        public List<string> Malzemeleri { get; set; }
+        public double Price { get; set; }
+
+
+        public void Pisir()
+        {
+            Console.WriteLine($"{GetType().Name} yemeği {PismeSuresi} dakika sürede pişti");
+        }
+
+        public virtual void SunumYap()
+        {
+            Console.WriteLine($"{GetType().Name} yemeği, yanında pilav ile sunuldu");
+
+        }
+    }
+
+    public class ZeytinYagliYemek : Yemek
+    {
+        public bool SogukMu { get; set; }
+        public bool LimonluMu { get; set; }
+
+        public override void SunumYap()
+        {
+            Console.WriteLine($"{GetType().Name} yemeği, yanında limon tabağı ile sunuldu");
+        }
+    }
+
+    public class EtliYemek : Yemek
+    {
+        public string PismeDerecesi { get; set; }
+        public bool SosVarMi { get; set; }
+
+        public void SosHazirla()
+        {
+
+        }
+    }
+
+    public class Kofte : EtliYemek
+    {
+
+    }
+
+    public class KuruKofte : Kofte
+    {
+
+    }
+
+
+}
+
---
+++ OOP/OOPOverview/Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Inheritance;
+
+Console.WriteLine("Hello, World!");
+
+Asci asci = new Asci();
+ZeytinYagliYemek pirasa = new ZeytinYagliYemek { PismeSuresi = 35 };
+KuruKofte kofte = new KuruKofte { PismeSuresi = 40 };
+
+asci.Pisir(kofte);
+asci.Pisir(pirasa);
+
